2253cfbe2ad2bd186bd15960bbb4079b98d677ec
Daniel Kinyo <kinyodaniel@gmail.com>
2023-01-22 03:13:13 +0300

    commit for deploy to dokku

--- app/controllers/concerns/onboarding_service.rb
+++ app/controllers/concerns/onboarding_service.rb
@@ -22,9 +22,9 @@ module OnboardingService
             p repo
             system("rm -rf ../#{dir_name}")
             p system("git clone #{repo} ../#{dir_name}")
-            system("cd /home/me/dev/dev3/beicloud/#{dir_name}")
+            p system("cd /home/me/dev/dev3/beicloud/#{dir_name} && pwd")
             # p system("cd #{dir_name}")
-            p system("pwd")
+            p "system(pwd)"
             p system("git add .")
             p system("git commit -m 'commit for deploy to dokku' ")
             # p system("git fetch --all")
